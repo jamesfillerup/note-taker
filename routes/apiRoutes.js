9a754b7aa081084d8bb98c35085f83e8d90cd90b
jamesfill <jamesp.fillerup@gmail.com>
2022-05-28 21:49:04 -0600

    not putting out html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,38 @@
-
+const path = require('path');
+const fs = require('fs')
 const router = require('express').Router();
-const noteData = require('../db/noteData');
+const uniqid = require('uniqid');
 
-router.get('/notes', function (req, res) {
-    noteData
-        .returnNotes()
-        .then(notes => res.json(notes))
-        .catch(err => res.status(500).json(err));
-});
+    router.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
 
-router.post('/notes', (req, res) => {
-    noteData
-        .addNote(req.body)
-        .then((note) => res.json(note))
-        .catch(err => res.status(500).json(err));
-});
 
-router.delete('/notes/:id', function (req, res) {
-    noteData
-        .deleteNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err));
-});
+    router.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
 
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uniqid(),
+        };
+
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+
+    });
+
+
+    router.delete('/api/notes/:id', (req, res) => {
+        
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
+        let deleteNotes = db.filter(item => item.id !== req.params.id);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+        res.json(deleteNotes);
+        
+    });
 
 module.exports = router;
