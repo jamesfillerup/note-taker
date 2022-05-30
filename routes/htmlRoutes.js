70389e0da113f50507845718143acdba7ddafeb6
jamesfill <jamesp.fillerup@gmail.com>
2022-05-30 11:20:00 -0600

    db and html working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const router = require('express').Router();
+const apiRoutes = require('./apiRoutes');
 
+router.use(apiRoutes);
 
     router.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
